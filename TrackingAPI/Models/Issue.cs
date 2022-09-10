e56887ca4047d711c7fbb161f6e9f8fbc8c7f4cb
MaxHarris76 <51440573+MaxHarris76@users.noreply.github.com>
2022-09-10 20:06:51 +0100

    Created  a simple web API API

---
+++ TrackingAPI/Models/Issue.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrackingAPI.Models
+{
+    public class Issue
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public Priority priority { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Completed { get; set; }
+    }
+
+    public enum Priority
+    {
+        Low, Medium, High
+    }
+}
